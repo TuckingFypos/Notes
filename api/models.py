36cacbb1b1fe2852cb0f5e340cb845aaa662b563
James DeLayo <TuckingFypos@users.noreply.github.com>
2020-02-10 17:23:29 -0500

    Builds more API end-points
    Adds in API end-points for calendar and organizing functionality, access to be implemented in github project 'Rose'

--- api/models.py
+++ api/models.py
@@ -4,7 +4,17 @@ class Note(models.Model):
 	title = models.CharField(max_length=200)
 	body = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
+	
+class Raid(models.Model):
+	title = models.TextField()
+	date = models.DateField()
+	time = models.DateTimeField()
+	instance = models.TextField()
+	
+class GameNight(models.Model):
+	organizer = models.TextField()
+	game_name = models.TextField()
+	time = models.DateTimeField()
 
 def __str__(self):
 	return '%s %s' % (self.title, self.body)
-
