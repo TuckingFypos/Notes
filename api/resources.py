5ddc630829e4e0c48ebe29746956672d47c0540e
James DeLayo <TuckingFypos@users.noreply.github.com>
2020-02-10 17:24:32 -0500

    Updates resources and queries
    Adds additional resources and queries for expanded functionality of Rose.

--- api/resources.py
+++ api/resources.py
@@ -1,11 +1,25 @@
 # api/resources.py
 
-from tastypie.resources import ModelResources
+from tastypie.resources import ModelResource
 from api.models import Note
+from api.models import Raid
+from api.models import GameNight
 from tastypie.authorization import Authorization
 
-class NoteResource(ModelResource)
+class NoteResource(ModelResource):
 	class Meta:
 		queryset = Note.objects.all()
 		resource_name = 'note'
+		authorization = Authorization()
+
+class RaidResource(ModelResource):
+	class Meta:
+		queryset = Raid.objects.all()
+		resource_name = 'raid'
+		authorization = Authorization()
+
+class GameNightResource(ModelResource):
+	class Meta:
+		queryset = GameNight.objects.all()
+		resource_name = 'game night'
 		authorization = Authorization()
